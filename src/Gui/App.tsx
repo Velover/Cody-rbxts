6d4760f2adae59614f50efd06ff6a89122df09df
Catargo <cobauper@gmail.com>
2025-04-06 02:00:49 +0200

    first commit

---
+++ src/Gui/App.tsx
@@ -0,0 +1,15 @@
+import React from "@rbxts/react";
+
+export default function App() {
+	return (
+		<frame
+			BackgroundColor3={new Color3(1, 1, 1)}
+			NextSelectionDown={undefined}
+			NextSelectionLeft={undefined}
+			NextSelectionRight={undefined}
+			NextSelectionUp={undefined}
+			SelectionImageObject={undefined}
+			Size={UDim2.fromScale(1.0, 1.0)}
+		></frame>
+	);
+}
